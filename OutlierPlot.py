cc38da58164d31cc9d5fdb1c72dc2e6568bcef33
damacdon <danikam1@uvic.ca>
2020-03-26 21:32:52 -0700

    Updated the parsing scheme to allow for more than 2 variables in the signal name, and added a function to plot the relative systs as bar plots for each signal point and channel

--- OutlierPlot.py
+++ OutlierPlot.py
@@ -1,23 +1,25 @@
 """
   Date:     200325
   History:  
-    - Originally written as a python notebook by Lukas Heinrich, and adapted for the 3L-RJ likelihood validation by Giordon Stark (https://github.com/kratsg/3L-RJ-mimic-likelihood-validation/blob/master/OutlierPlot.ipynb)
+    - Originally written as a python notebook by Lukas Heinrich
+    - Adapted for the 3L-RJ likelihood validation by Giordon Stark (https://github.com/kratsg/3L-RJ-mimic-likelihood-validation/blob/master/OutlierPlot.ipynb)
     - Adapted and generalized to script by Danika MacDonell [March 25, 2020]
     
-  Details:  Script to visualize the fractional size of the systematics for pyhf likelihoods at each mass point, where the systematics are added together in quadrature. Should be run in a directory containing the background-only json likelihood file, along with a patch json likelihood file for each signal point. Assumes that the model has two variable mass points, and the signals in the json patch files are named as such.
+  Details:  Script to visualize the relative size of the systematics for pyhf likelihoods at each mass point, where the systematics are added together in quadrature. Should be run in a directory containing the background-only json likelihood file, along with a patch json likelihood file for each signal point. Assumes that all the json patch file are located at the same directory level as this script.
   
   Usage:
-  >> python OutlierPlot.py --signal_template <signal_template_{}_{}_for_masses> --v_max <max colourbar amplitude> --x_label <x axis label> --y_label <y axis label>
+  >> python OutlierPlot.py --signal_template <signal_template_{a}_{b}_{c}_for_masses> --x_var <which variable in signal name template to plot on x axis (defaults to 'a')> --y_var <which variable in signal name template to plot on x axis (defaults to 'b')> --v_max <max colourbar amplitude> --x_label <x axis label> --y_label <y axis label>
   
   Example for 1Lbb Wh analysis (https://glance.cern.ch/atlas/analysis/analyses/details.php?id=2969):
   
-  >> python OutlierPlot.py --signal_template C1N2_Wh_hbb_{}_{} --v_max 10 --x_label '$m(\tilde{\chi}_{1}^{\pm}/\tilde{\chi}_{2}^{0})$ [GeV]' --y_label '$m(\tilde{\chi}_{1}^{0})$ [GeV]'
+  >> python OutlierPlot.py --signal_template C1N2_Wh_hbb_{a}_{b} --x_var a --y_var b --v_max 10 --x_label '$m(\tilde{\chi}_{1}^{\pm}/\tilde{\chi}_{2}^{0})$ [GeV]' --y_label '$m(\tilde{\chi}_{1}^{0})$ [GeV]'
   
   The signal template is the name of an arbitrary signal in the json patch files, with the signal masses left as {}. Given a background-only file and a patch file, the signal name can be found under "samples" in the output of:
   >> jsonpatch BkgOnly.json patch_XXX.json | pyhf inspect
   
   If, for example, one of the signals is called C1N2_Wh_hbb_550_200, where 550 and 200 are the variable model masses, the signal template would be C1N2_Wh_hbb_{}_{}.
   """
+
 import json
 import glob
 import numpy as np
@@ -26,8 +28,8 @@ import scipy.interpolate
 from parse import *
 import click
 import pyhf
-plt.rc('xtick', labelsize=18)
-plt.rc('ytick', labelsize=18)
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
 
 def handle_deltas(delta_up, delta_dn):
     nom_is_center = np.bitwise_or(
@@ -42,64 +44,122 @@ def handle_deltas(delta_up, delta_dn):
 def process_patch(p):
     nom = np.asarray(p['value']['data'])
 
-
     #histosys
     hid = np.asarray([m['data']['hi_data'] for m in p['value']['modifiers'] if m['type'] == 'histosys'])
     lod = np.asarray([m['data']['lo_data'] for m in p['value']['modifiers'] if m['type'] == 'histosys'])
     delta_up = hid-nom
     delta_dn = nom-lod
-    A = handle_deltas(delta_up,delta_dn)
+    histo_deltas = handle_deltas(delta_up,delta_dn)
 
     hi = np.asarray([m['data']['hi'] for m in p['value']['modifiers'] if m['type'] == 'normsys'])
     lo = np.asarray([m['data']['lo'] for m in p['value']['modifiers'] if m['type'] == 'normsys'])
     delta_up = np.asarray([h*nom-nom for h in hi])
     delta_dn = np.asarray([l*nom-nom for l in lo])
-    B = handle_deltas(delta_up,delta_dn)
-
+    norm_deltas = handle_deltas(delta_up,delta_dn)
 
     delta = np.asarray([m['data'] for m in p['value']['modifiers'] if m['type'] == 'staterror'])
     delta_up = (delta/2.)
     delta_dn = (delta/2.)
-    C = handle_deltas(delta_up,delta_dn)
-    C = np.zeros_like(A)
+    stat_deltas = handle_deltas(delta_up,delta_dn)
+    stat_deltas = np.zeros_like(histo_deltas)      # Don't consider the stat error for this calculation
 
-    systs = np.concatenate([A,B,C])
+    systs = np.concatenate([histo_deltas, norm_deltas, stat_deltas])
     inquad = np.sqrt(np.sum(np.square(systs), axis=0))
     rel =  inquad/nom
     rel = np.where(nom == 0, np.ones_like(nom), rel)
     return rel,nom
 
+def plot_rel_systs(p, channel_names, channel_bins):
+  
+    signal_name = p['value']['name']
+    channel_name = channel_names[p['path']]
+    nom = np.asarray(p['value']['data'])
+    n_bins = channel_bins[p['path']]
+
+    # Collect syst names and deltas for histo systs
+    sys_names_histo = [np.asarray([m['name'] for m in p['value']['modifiers'] if m['type'] == 'histosys'])][0].tolist()
+    hid = np.asarray([m['data']['hi_data'] for m in p['value']['modifiers'] if m['type'] == 'histosys'])
+    lod = np.asarray([m['data']['lo_data'] for m in p['value']['modifiers'] if m['type'] == 'histosys'])
+    delta_up = hid-nom
+    delta_dn = nom-lod
+    histo_deltas = handle_deltas(delta_up, delta_dn)
+    histo_rel_size = (np.absolute(histo_deltas)/(1.0*nom))
+    
+    # Collect syst names and deltas for norm systs
+    sys_names_norm = [np.asarray([m['name'] for m in p['value']['modifiers'] if m['type'] == 'normsys'])][0].tolist()
+    hi = np.asarray([m['data']['hi'] for m in p['value']['modifiers'] if m['type'] == 'normsys'])
+    lo = np.asarray([m['data']['lo'] for m in p['value']['modifiers'] if m['type'] == 'normsys'])
+    delta_up = np.asarray([h*nom-nom for h in hi])
+    delta_dn = np.asarray([l*nom-nom for l in lo])
+    norm_deltas = handle_deltas(delta_up,delta_dn)
+    norm_rel_size = (np.absolute(norm_deltas)/(1.0*nom))
+    
+    sys_names = sys_names_histo + sys_names_norm
+    
+    for iBin in range(n_bins):
+        bin_number = iBin+1
+        rel_size = np.concatenate((histo_rel_size[:,iBin], norm_rel_size[:,iBin]))
+        sys_names_bin = [sys_names[idx] for idx, size in enumerate(rel_size) if size > 0.5]
+        rel_size = rel_size[rel_size > 0.5]
+        
+        # Only make a plot if there are any relative systematics above 0.5
+        if len(sys_names_bin) > 0:
+            x = np.arange(len(sys_names_bin))   # the label locations
+            width = 0.5  # the width of the bars
+            fig = plt.figure(figsize =(12, 6)); ax=fig.add_subplot(1,1,1)
+            ax.set_ylabel('Relative Syst Size', fontsize=16)
+            ax.set_title(f'{signal_name}: {channel_name} (Bin {bin_number}), Norm & Histo', fontsize=20)
+            bar = ax.bar(x - width/2, rel_size, width)
+            ax.set_xticks(x)
+            ax.set_xticklabels(sys_names_bin, rotation=45, ha='right')
+            plt.tight_layout()
+            plt.savefig(f'Plots/rel_systs_{signal_name}_{channel_name}_bin{bin_number}.png')
+            plt.close()
+
+
 @click.command()
 @click.option(
               "--signal_template",
-              help="Signal name template, with 2 signal masses as variables (eg. signal_{}_{}_more_info)",
+              help="Signal name template, with signal masses as variables. Signal masses must be denoted by {a}, {b}, {c}, ... (eg. signal_{a}_{b}_more_info)",
+              )
+@click.option(
+              "--x_var",
+              help="Which variable from the signal name template to plot on the x axis (defaults to 'a')",
+              default='a',
+              required=False
+              )
+@click.option(
+              "--y_var",
+              help="Which variable from the signal name template to plot on the y axis (defaults to 'b')",
+              default='b',
+              required=False
               )
 @click.option(
               "--v_max",
-              help="Maximum amplitude of the colourbar for plotting interpolated fractional syst sizes",
+              help="Maximum amplitude of the colourbar for plotting interpolated relative syst sizes",
               default=10,
               required=False
               )
 @click.option(
               "--x_label",
-              help="x label for interpolated plot of fractional systematics",
+              help="x label for interpolated plot of relative systematics",
               default=None,
               required=False
               )
 
 @click.option(
               "--y_label",
-              help="y label for interpolated plot of fractional systematics",
+              help="y label for interpolated plot of relative systematics",
               default=None,
               required=False
               )
-def outlier_plot(signal_template, v_max, x_label, y_label):
+def outlier_plot(signal_template, v_max, x_var, y_var, x_label, y_label):
 
     patches = [json.load(open(x)) for x in glob.glob('patch*.json')]
     grouped_patches = {x[0]['value']['name']:{p['path']:p for p in x if p['op'] == 'add'} for x in patches if 'value' in x[0]}
 
-
     data = {x[0]['value']['name']:{p['path']:process_patch(p) for p in x if p['op'] == 'add'} for x in patches if 'value' in x[0]}
+
     #print(data)
 
     # Make the mapping of json channel names to analysis region names
@@ -128,6 +188,14 @@ def outlier_plot(signal_template, v_max, x_label, y_label):
                 if r > 1.0:
                     outliers.append((k,kk,b,r,n))
 
+
+    # Make plots of relative syst for each signal point and bin
+    for x in patches:
+        if 'value' in x[0]:
+            for p in x:
+                if p['op'] == 'add':
+                    plot_rel_systs(p, channel_names, channel_bins)
+    
     #print("Outliers (> 1.0):")
     #for o in list(reversed(sorted(outliers, key = lambda x: x[-1]))):
     #    print('\t',o[-1],o[-2],o[0],channel_names[o[1]],o[2])
@@ -147,12 +215,12 @@ def outlier_plot(signal_template, v_max, x_label, y_label):
     sig_name_template = signal_template
 
     for ichan, channel in enumerate(channels_json):
-        frac_systs = np.asarray(
-            [[float(m) for m in [parse(sig_name_template, k)[0], parse(sig_name_template, k)[1]]] + v.get(channel, np.array([0.0]*channel_bins[channel])).tolist() for k,v in data.items()
+        rel_systs = np.asarray(
+            [[float(m) for m in [parse(sig_name_template, k).named[x_var], parse(sig_name_template, k).named[y_var]]] + v.get(channel, np.array([0.0]*channel_bins[channel])).tolist() for k,v in data.items()
             ]
         )
-        x_min, x_max = min(frac_systs[:,0]), max(frac_systs[:,0])
-        y_min, y_max = min(frac_systs[:,1]), max(frac_systs[:,1])
+        x_min, x_max = min(rel_systs[:,0]), max(rel_systs[:,0])
+        y_min, y_max = min(rel_systs[:,1]), max(rel_systs[:,1])
         x,y = np.mgrid[x_min:x_max:100j, y_min:y_max:100j]
         for jbin in range(2,2+channel_bins[channel]):
             f = plt.figure(figsize =(12, 6)); ax=f.add_subplot(1,1,1)
@@ -161,14 +229,14 @@ def outlier_plot(signal_template, v_max, x_label, y_label):
             ax.set_xlim(x_min-25, x_max+25)
             ax.set_ylim(y_min-25, y_max+25)
             bin_number = jbin - 1
-            frac_systs = frac_systs[frac_systs[:, jbin] != 0]   # Remove any points with zero amplitude
+            rel_systs = rel_systs[rel_systs[:, jbin] != 0]   # Remove any points with zero relative syst
             
-            z = scipy.interpolate.griddata(frac_systs[:,:2],frac_systs[:,jbin],(x,y))
-            #print(frac_systs)
+            z = scipy.interpolate.griddata(rel_systs[:,:2],rel_systs[:,jbin],(x,y))
+            #print(rel_systs)
             
             vmin,vmax = 0, v_max
             #z = np.log(z)
-            ax.scatter(frac_systs[:,0],frac_systs[:,1], c = frac_systs[:,jbin], edgecolors='w', vmin = vmin, vmax = vmax)
+            ax.scatter(rel_systs[:,0],rel_systs[:,1], c = rel_systs[:,jbin], edgecolors='w', vmin = vmin, vmax = vmax)
             im = ax.contourf(x,y,z, levels = np.linspace(vmin,vmax,100))
             cb = plt.colorbar(im, ax=ax)
             cb.set_label(label='$\oplus$ (histosys, normsys, staterr)', fontsize=18)
@@ -176,7 +244,7 @@ def outlier_plot(signal_template, v_max, x_label, y_label):
             else: ax.set_title(f'{channel_names[channel]} (Bin {bin_number})', fontsize=20)
             
             outliers_chan = np.asarray(
-                [[float(parse(sig_name_template, o[0])[0]), float(parse(sig_name_template, o[0])[1])] + [o[-2]] for o in outliers
+                [[float(parse(sig_name_template, o[0]).named[x_var]), float(parse(sig_name_template, o[0]).named[y_var])] + [o[-2]] for o in outliers
                  if o[1] == channel and o[2] == jbin-2]
             )
             
@@ -188,7 +256,7 @@ def outlier_plot(signal_template, v_max, x_label, y_label):
                 ax.text(o[0]+5,o[1]+5,'{:.2f}'.format(o[2]), c='r')
         
             plt.tight_layout()
-            plt.savefig(f'{channel_names[channel]}_bin{bin_number}.png')
+            plt.savefig(f'Plots/{channel_names[channel]}_bin{bin_number}.png')
             plt.close()
 
 if __name__ == "__main__":
